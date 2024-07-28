d94aa5f733142ddda55732ba211a3d6214ef0027
Axel GMX <agmx183@gmail.com>
2024-07-29 01:14:27 +0200

    App initialization : Routes - Home + Page404

--- p13_argent_bank_Front-end/src/app/App.js
+++ p13_argent_bank_Front-end/src/app/App.js
@@ -1,25 +1,32 @@
-import logo from '../logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Page404 from "../pages/Page404/Page404";
 
-function App() {
+/**
+ * App is a React component that represents the main application router.
+ * It uses the `BrowserRouter` component from the `react-router-dom` library
+ * to define the routing structure of the application.
+ *
+ * The router configuration includes the following routes:
+ * - The root path ("/") which renders the `Home` component.
+ * - A wildcard path ("*") which renders the `Page404` component for any undefined routes.
+ * - A specific path ("/error") which also renders the `Page404` component for error handling.
+ *
+ * @returns {JSX.Element} The application routes wrapped in a BrowserRouter.
+ */
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
+          <Route path="/error" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
